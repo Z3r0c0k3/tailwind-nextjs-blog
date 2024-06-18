a4eeeb1e00661e087020cbe363355f345352eb57
Jeon woojin <geckobaem@gmail.com>
2024-06-18 11:21:47 +0900

    next.config.js assetPrefix ADDED 2

--- next.config.js
+++ next.config.js
@@ -66,7 +66,7 @@ module.exports = () => {
     eslint: {
       dirs: ["app", "components", "layouts", "scripts"],
       },
-    // output: 'export',
+    output: 'export',
     assetPrefix: '/out',
     images: {
       remotePatterns: [
